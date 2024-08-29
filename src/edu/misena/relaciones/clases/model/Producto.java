a394cbb32017c3adef7ebd15acb6a98c290fb53e
Andreagaona14 <andreitagaona05@gmail.com>
2024-08-29 06:17:39 -0500

    Correccion de la clase Producto

--- src/edu/misena/relaciones/clases/model/Producto.java
+++ src/edu/misena/relaciones/clases/model/Producto.java
@@ -5,9 +5,19 @@ public class Producto {
     private String nombre;
     private double precio;
 
+    /*
+
+    El último código asignado a un producto.
+    Este campo se usa para generar nuevos códigos únicos para cada instancia de {@code Producto}.*/
+    private static int ultimoCodigo = 0;
+
+
+    //TAREA: Crea un constructor que asigna automáticamente un código único al producto incrementando {@code ultimoCodigo}.*/
+
     //Constructor
-    public Producto(String codigo, String nombre, double precio){
-        this.codigo = codigo;
+    public Producto(String nombre, double precio){
+        ultimoCodigo++;
+        this.codigo = "PROD" +ultimoCodigo;
         this.nombre = nombre;
         this.precio = precio;
     }
@@ -37,5 +47,16 @@ public class Producto {
     public void setPrecio(double precio){
         this.precio = precio;
     }
+
+    /*Devuelve una representación en cadena del producto.
+     <p>
+     La representación incluye el código, nombre y precio del producto, separados por tabulaciones.
+     </p>
+     *
+     @return Una cadena que representa el producto.
+     */
+    @Override
+    public String toString() {
+        return codigo + "\t" + nombre + "\t" + precio;}
 
 }
